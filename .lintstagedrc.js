085bf043888debb3b8e8f33a05cec9a550cc8af9
rishab raj verma <rishabrajv@mindfiresolutions.com>
2026-05-20 22:07:07 +0530

    feat: add commit lints

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,4 @@
 module.exports = {
-  // Run Next.js built-in linter cleanly on modified JS/TS files
-  '*.{js,jsx,ts,tsx}': ['next lint --dir app --dir components --dir lib'],
-
-  // Format modified styles, markdown, and configurations
-  '*.{json,css,md}': ['prettier --write'],
+  // Format modified code, styles, and configurations seamlessly
+  '*.{js,jsx,ts,tsx,json,css,md}': ['prettier --write'],
 };
